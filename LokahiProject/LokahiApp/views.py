cf076590d6055b63fd616a5f51b9f566a58a40f4
Richard Dizon <rcd6ab@virginia.edu>
2017-04-04 15:45:01 -0400

    Create additional views and fix urls
    * Add number to notes
    
    * change title in login
    
    * Created several new views/ reassigned several html files
    
    * create reports model and views
    
    * reports page styling
    
    * Added navigational buttons to several views
    
    * fix url problem

--- LokahiProject/LokahiApp/views.py
+++ LokahiProject/LokahiApp/views.py
@@ -2,11 +2,19 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
-
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Report
 
 # disabling csrf (cross site request forgery)
 @csrf_exempt
 def index(request):
+        # if post request is not true
+        # returing the form template
+        template = loader.get_template('index.html')
+        return HttpResponse(template.render())
+
+def login(request):
     # if post request came
     if request.method == 'POST':
         # getting values from post
@@ -20,12 +28,32 @@ def index(request):
         }
 
         # getting our showdata template
-        template = loader.get_template('showdata.html')
+        template = loader.get_template('LoginForm.html')
 
         # returing the template
         return HttpResponse(template.render(context, request))
-    else:
+
+    else: 
         # if post request is not true
         # returing the form template
-        template = loader.get_template('index.html')
+        template = loader.get_template('login.html')
         return HttpResponse(template.render())
+
+
+def homepage(request):
+        template = loader.get_template('homepage.html')
+        return HttpResponse(template.render())
+
+
+def create_report(request):
+        template = loader.get_template('create_report.html')
+        return HttpResponse(template.render())
+
+
+def report(request):
+    reports = Report.objects.filter(timestamp__lte=timezone.now()).order_by('timestamp')
+    return render(request, 'report.html', {'reports': reports})
+
+
+
+
