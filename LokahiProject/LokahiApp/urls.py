4e058219e1588afe0a60077a5d32afbc41761219
Richard Dizon <rcd6ab@virginia.edu>
2017-04-05 17:01:47 -0400

    Added log-in functionality

--- LokahiProject/LokahiApp/urls.py
+++ LokahiProject/LokahiApp/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/', views.login),
+    url(r'^login/', auth_views.login, name='login'),
     url(r'^homepage/', views.homepage),
     url(r'^create_report/', views.create_report),
     url(r'^report/', views.report),
