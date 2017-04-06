c046b74d4a3c0f5c9ed0c3778e152619bb215566
Richard Dizon <rcd6ab@virginia.edu>
2017-04-06 13:15:11 -0400

    Implement log-out

--- LokahiProject/LokahiApp/urls.py
+++ LokahiProject/LokahiApp/urls.py
@@ -7,6 +7,8 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', auth_views.login, name='login'),
+    # Logging out takes you back to the landing page
+    url(r'^logout/$', auth_views.logout, {'template_name': 'index.html'}, name='logout'),
     url(r'^homepage/', views.homepage),
     url(r'^create_report/', views.create_report),
     url(r'^report/', views.report),
