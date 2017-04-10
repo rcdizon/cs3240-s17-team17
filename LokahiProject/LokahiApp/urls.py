9eb06ecf9f3fa67ce749020338d917404c184ffb
Richard Dizon <rcd6ab@virginia.edu>
2017-04-09 23:27:20 -0400

    Added log-in functionality
    * Added log-in functionality
    
    * Update user db
    
    * Implement log-out
    
    * Default URL redirects to landing page

--- LokahiProject/LokahiApp/urls.py
+++ LokahiProject/LokahiApp/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/', views.login),
+    url(r'^login/', auth_views.login, name='login'),
+    # Logging out takes you back to the landing page
+    url(r'^logout/$', auth_views.logout, {'template_name': 'index.html'}, name='logout'),
     url(r'^homepage/', views.homepage),
     url(r'^create_report/', views.create_report),
     url(r'^report/', views.report),
