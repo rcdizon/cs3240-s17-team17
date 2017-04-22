1fcde0cc01d198cd5d54feebfb9a077b2c782cf2
Nikki Pope <np4ft@virginia.edu>
2017-04-22 12:24:27 -0400

    changed settings.py to database name on Nikki's computer

--- LokahiProject/LokahiProject/settings.py
+++ LokahiProject/LokahiProject/settings.py
@@ -75,12 +75,16 @@ WSGI_APPLICATION = 'LokahiProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {                                                                    
+    'default': {                                                                 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',                      
+        'NAME': 'postgres',                                                   
+        'USER': 'postgres',                                                   
+        'PASSWORD': 'mypost',                                      
+        'HOST': '127.0.0.1',                                                     
+        'PORT': '5432',                                                          
+    }                                                                            
+} 
 
 
 # Password validation
