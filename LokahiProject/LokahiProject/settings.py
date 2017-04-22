319c4ef198277ea4685596a9395b00c244b7e28b
Nikki Pope <np4ft@virginia.edu>
2017-04-22 11:29:58 -0400

    Initial database settings changes

--- LokahiProject/LokahiProject/settings.py
+++ LokahiProject/LokahiProject/settings.py
@@ -75,12 +75,16 @@ WSGI_APPLICATION = 'LokahiProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {                                                                    
+    'default': {                                                                 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',                      
+        'NAME': 'db',                                                   
+        'USER': 'my_database_user',                                                   
+        'PASSWORD': '',                                      
+        'HOST': 'localhost',                                                     
+        'PORT': '5432',                                                          
+    }                                                                            
+} 
 
 
 # Password validation
