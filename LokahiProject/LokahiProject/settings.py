a75b5754ca1cb57a9587e7c780992574e422efa2
Richard Dizon <rcd6ab@virginia.edu>
2017-04-24 11:40:01 -0400

    Fixed static settings

--- LokahiProject/LokahiProject/settings.py
+++ LokahiProject/LokahiProject/settings.py
@@ -31,7 +31,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
