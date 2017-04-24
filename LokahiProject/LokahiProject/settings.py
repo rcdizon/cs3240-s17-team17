3de97a1ea5e57baf45535fd2a3750f31f2faf6c4
Richard Dizon <rcd6ab@virginia.edu>
2017-04-24 11:58:18 -0400

    More settings changes

--- LokahiProject/LokahiProject/settings.py
+++ LokahiProject/LokahiProject/settings.py
@@ -15,9 +15,9 @@ import dj_database_url
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
