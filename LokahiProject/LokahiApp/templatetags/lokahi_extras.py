294ec54009f13595a01499edb877af021bdba905
Richard Dizon <rcd6ab@virginia.edu>
2017-05-02 18:11:03 -0400

    Bootstrapify pages (#42)
    * redoing initial bootstrapping
    
    * initial card-style reports
    
    * index card revamp
    
    * Replace Inbox button
    
    * Provided css online and refactor links
    
    * Fix header colors
    
    * 'FinTech' not 'Fin Tech'
    
    * Fix groups logic
    
    * Added missing files
    
    * landing page revamp
    
    * minor changes to group_successful, groups, inbox

--- LokahiProject/LokahiApp/templatetags/lokahi_extras.py
+++ LokahiProject/LokahiApp/templatetags/lokahi_extras.py
@@ -6,4 +6,6 @@ register = template.Library()
 @register.filter(name='has_group')
 def has_group(user, group_name):
     group = Group.objects.get(id=group_name)
+    if user == None:
+        return False
     return True if group in user.groups.all() else False
